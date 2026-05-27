ae2fc76b1f600d41b56e5e63289c096236b7e2ce
Mone <2164711521@qq.com>
2026-05-27 22:13:00 +0800

    修改了一些基础设置

--- 1-1_InitSystemClock/Core/User/main.c
+++ 1-1_InitSystemClock/Core/User/main.c
@@ -1,5 +1,13 @@
 #include "stm32f1xx_hal.h"
 
+uint32_t HCLKFreq;
+uint32_t PCLK1Freq;
+uint32_t PCLK2req;
+
 int main(void) {
-    
+    HAL_Init();
+    SystemCoreClockUpdate();
+    HCLKFreq = HAL_RCC_GetHCLKFreq();
+    PCLK1Freq = HAL_RCC_GetPCLK1Freq();
+    PCLK2req = HAL_RCC_GetPCLK2Freq();
 }
