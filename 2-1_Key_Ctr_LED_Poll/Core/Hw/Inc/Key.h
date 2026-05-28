2c55f79dcbb378765fef2ca5a7b6075318b2a0ad
Mone <2164711521@qq.com>
2026-05-28 18:16:59 +0800

    按键控制LED(长短按模式测试正常，延迟模式待完善)

---
+++ 2-1_Key_Ctr_LED_Poll/Core/Hw/Inc/Key.h
@@ -0,0 +1,15 @@
+#ifndef __KEY_H
+#define __KEY_H
+
+#define Key1_Val HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1)
+#define Key2_Val HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11)
+
+typedef enum {
+    Down = 1,
+    Up = 0
+} Key_Mode;
+
+void Key_Init(void);
+uint8_t Key_Val(Key_Mode Mode);
+
+#endif
