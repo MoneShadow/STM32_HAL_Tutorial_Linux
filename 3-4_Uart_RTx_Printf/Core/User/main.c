0fd8ecc247d6f707ed9c2a355db17f22cc6c6d75
Mone <2164711521@qq.com>
2026-06-04 14:25:42 +0800

    3-4 串口重映射prinf函数

---
+++ 3-4_Uart_RTx_Printf/Core/User/main.c
@@ -0,0 +1,22 @@
+#include "stm32f1xx_hal.h"
+#include "rcc.h"
+#include "uart.h"
+
+int main(void) {
+    HAL_Init();
+    RCC_InitClock();
+    UART2_Init();
+
+    while (1) {
+        if (status == 1 && uart2.gState == HAL_UART_STATE_READY) {
+            status = 0;
+            uint8_t vallen = rb.buffer[rb.tail], j = 0, byte = 0;
+            rb.tail = NEXT_IDX(rb.tail, (&rb));
+            while (vallen-- != 0) {
+                R_ReadByte(&rb, &byte);
+                txbuffer2[j++] = byte;
+            }
+            HAL_UART_Transmit_DMA(&uart2, txbuffer2, j);
+        }
+	}
+}
