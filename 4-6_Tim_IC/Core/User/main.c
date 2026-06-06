cc02b155a87f620d3992b9000a569d75e1accd58
Mone <2164711521@qq.com>
2026-06-06 22:29:47 +0800

    4-5 定时器输入捕获功能(Poll IT DMA)

---
+++ 4-6_Tim_IC/Core/User/main.c
@@ -0,0 +1,16 @@
+#include "stm32f1xx_hal.h"
+#include "rcc.h"
+#include "uart.h"
+#include "tim.h"
+#include <stdint.h>
+
+int main(void) {
+    HAL_Init();
+    RCC_InitClock();
+    UART2_Init();
+    Timer1_Init(4, 0, 0);
+    
+    while (1) {
+        
+	}
+}
