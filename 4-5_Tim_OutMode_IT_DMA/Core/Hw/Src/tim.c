a8c5f9bb8c01e351d2f62da543043febc6e3a0bd
Mone <2164711521@qq.com>
2026-06-06 20:27:19 +0800

    4-5 定时器外部时钟模式中断和DMA

---
+++ 4-5_Tim_OutMode_IT_DMA/Core/Hw/Src/tim.c
@@ -0,0 +1,73 @@
+#include "stm32f103xb.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_dma.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_tim.h"
+#include <stdint.h>
+#include "tim.h"
+#include "uart.h"
+
+TIM_HandleTypeDef Tim_InitStructure;
+TIM_ClockConfigTypeDef Tim_Clock;
+
+void Timer1_Init(uint16_t arr, uint16_t psc, uint8_t rep) {
+    Tim_InitStructure.Instance = TIM1;
+    Tim_InitStructure.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+    Tim_InitStructure.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    Tim_InitStructure.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
+    Tim_InitStructure.Init.RepetitionCounter = rep;
+    Tim_InitStructure.Init.Period = arr;
+    Tim_InitStructure.Init.Prescaler = psc;
+
+    Tim_Clock.ClockSource = TIM_CLOCKSOURCE_ETRMODE1;
+    Tim_Clock.ClockPrescaler = TIM_CLOCKPRESCALER_DIV1;
+    Tim_Clock.ClockFilter = 0x3;
+    Tim_Clock.ClockPolarity = TIM_ETRPOLARITY_INVERTED;
+    
+    HAL_TIM_Base_Init(&Tim_InitStructure);
+    HAL_TIM_ConfigClockSource(&Tim_InitStructure, &Tim_Clock);
+    __HAL_TIM_CLEAR_FLAG(&Tim_InitStructure, TIM_FLAG_UPDATE);
+
+    //HAL_Delay(50);
+    //__HAL_TIM_CLEAR_FLAG(&Tim_InitStructure, TIM_FLAG_TRIGGER);
+    //__HAL_TIM_SET_COUNTER(&Tim_InitStructure, 0);
+
+    HAL_TIM_Base_Start(&Tim_InitStructure);
+}
+
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim) {
+    GPIO_InitTypeDef GPIO_Init_ST;
+
+    if (htim->Instance == TIM1) {
+        __HAL_RCC_TIM1_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        GPIO_Init_ST.Mode = GPIO_MODE_INPUT;
+        GPIO_Init_ST.Pin = GPIO_PIN_12;
+        GPIO_Init_ST.Pull = GPIO_PULLDOWN;
+        HAL_GPIO_Init(GPIOA, &GPIO_Init_ST);
+    }
+    else if (htim->Instance == TIM2) {
+        __HAL_RCC_TIM2_CLK_ENABLE();
+    }
+    else if (htim->Instance == TIM3) {
+        __HAL_RCC_TIM3_CLK_ENABLE();
+    }
+    else if (htim->Instance == TIM4) {
+        __HAL_RCC_TIM4_CLK_ENABLE();
+    }
+}
+
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == TIM1) {
+        __HAL_RCC_TIM1_CLK_DISABLE();
+    }
+    else if (htim->Instance == TIM2) {
+        __HAL_RCC_TIM2_CLK_DISABLE();
+    }
+    else if (htim->Instance == TIM3) {
+        __HAL_RCC_TIM3_CLK_DISABLE();
+    }
+    else if (htim->Instance == TIM4) {
+        __HAL_RCC_TIM4_CLK_DISABLE();
+    }
+}
