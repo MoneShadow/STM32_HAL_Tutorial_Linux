1b54646f55c49f07de8c80c483f0a1865c8310c7
Mone <2164711521@qq.com>
2026-06-07 17:28:20 +0800

    直连模式实现两次触发间隔计时DMA

--- 4-6_Tim_IC/Core/Hw/Src/stm32f1xx_it.c
+++ 4-6_Tim_IC/Core/Hw/Src/stm32f1xx_it.c
@@ -173,6 +173,10 @@ void USART2_IRQHandler(void) {
 	}
 }
 
+void DMA1_Channel2_IRQHandler(void) {
+  HAL_DMA_IRQHandler(&DMA_ST);
+}
+
 void DMA1_Channel5_IRQHandler(void) {
   HAL_DMA_IRQHandler(&DMA_ST);
 }
--- 4-6_Tim_IC/Core/Hw/Src/tim.c
+++ 4-6_Tim_IC/Core/Hw/Src/tim.c
@@ -12,7 +12,8 @@ TIM_IC_InitTypeDef Tim_InitIC;
 DMA_HandleTypeDef DMA_ST;
 
 uint16_t updatacount = 0;
-uint16_t dmabuffer[4] = {6, 7, 8, 9};
+uint16_t dmabuffer[2];
+uint16_t Times1, Times2;
 
 void Timer1_Init(uint16_t arr, uint16_t psc, uint8_t rep) {
     Tim_InitStructure.Instance = TIM1;
@@ -30,24 +31,27 @@ void Timer1_Init(uint16_t arr, uint16_t psc, uint8_t rep) {
     Tim_InitIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
     HAL_TIM_IC_ConfigChannel(&Tim_InitStructure, &Tim_InitIC, TIM_CHANNEL_1);
 
-    Tim_InitIC.ICPrescaler = TIM_ICPSC_DIV1;
-    Tim_InitIC.ICFilter = 0x8;
-    Tim_InitIC.ICPolarity = TIM_ICPOLARITY_FALLING;
-    Tim_InitIC.ICSelection = TIM_ICSELECTION_INDIRECTTI;
-    HAL_TIM_IC_ConfigChannel(&Tim_InitStructure, &Tim_InitIC, TIM_CHANNEL_2);
+    //Tim_InitIC.ICPrescaler = TIM_ICPSC_DIV1;
+    //Tim_InitIC.ICFilter = 0x8;
+    //Tim_InitIC.ICPolarity = TIM_ICPOLARITY_FALLING;
+    //Tim_InitIC.ICSelection = TIM_ICSELECTION_INDIRECTTI;
+    //HAL_TIM_IC_ConfigChannel(&Tim_InitStructure, &Tim_InitIC, TIM_CHANNEL_2);
 
     __HAL_TIM_CLEAR_FLAG(&Tim_InitStructure, TIM_FLAG_UPDATE);
 
     HAL_NVIC_SetPriority(TIM1_UP_IRQn, 3, 0);
     HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
 
-    HAL_NVIC_SetPriority(TIM1_CC_IRQn, 3, 0);
-    HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
+    HAL_NVIC_SetPriority(DMA1_Channel2_IRQn, 2, 0);
+    HAL_NVIC_EnableIRQ(DMA1_Channel2_IRQn);
+
+    //HAL_NVIC_SetPriority(TIM1_CC_IRQn, 3, 0);
+    //HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
 
     __HAL_TIM_ENABLE_IT(&Tim_InitStructure, TIM_FLAG_UPDATE);
 
-    HAL_TIM_IC_Start_IT(&Tim_InitStructure, TIM_CHANNEL_1);
-    HAL_TIM_IC_Start_IT(&Tim_InitStructure, TIM_CHANNEL_2);
+    HAL_TIM_IC_Start_DMA(&Tim_InitStructure, TIM_CHANNEL_1, (uint32_t *)dmabuffer, 2);
+    //HAL_TIM_IC_Start_IT(&Tim_InitStructure, TIM_CHANNEL_2);
 }
 
 void HAL_TIM_IC_MspInit(TIM_HandleTypeDef *htim) {
@@ -56,11 +60,23 @@ void HAL_TIM_IC_MspInit(TIM_HandleTypeDef *htim) {
     if (htim->Instance == TIM1) {
         __HAL_RCC_TIM1_CLK_ENABLE();
         __HAL_RCC_GPIOA_CLK_ENABLE();
+        __HAL_RCC_DMA1_CLK_ENABLE();
 
         GPIO_Init_ST.Mode = GPIO_MODE_INPUT;
         GPIO_Init_ST.Pin = GPIO_PIN_8 | GPIO_PIN_9;
         GPIO_Init_ST.Pull = GPIO_PULLDOWN;
         HAL_GPIO_Init(GPIOA, &GPIO_Init_ST);
+
+        DMA_ST.Instance = DMA1_Channel2;
+        DMA_ST.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+        DMA_ST.Init.MemInc = DMA_MINC_ENABLE;
+        DMA_ST.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+        DMA_ST.Init.PeriphInc = DMA_PINC_DISABLE;
+        DMA_ST.Init.Direction = DMA_PERIPH_TO_MEMORY;
+        DMA_ST.Init.Mode = DMA_CIRCULAR;
+        DMA_ST.Init.Priority = DMA_PRIORITY_MEDIUM;
+        __HAL_LINKDMA(&Tim_InitStructure, hdma[TIM_DMA_ID_CC1], DMA_ST);
+        HAL_DMA_Init(&DMA_ST);
     }
     else if (htim->Instance == TIM2) {
         __HAL_RCC_TIM2_CLK_ENABLE();
@@ -88,7 +104,7 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim) {
     }
 }
 
-/* 更新中断回调 DMA全完成中断完成回调 */
+/* 更新中断回调 DMA完成中断回调 */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     if (htim->Instance == TIM1) {
         u2_prinf("Tim1Updata: %d\r\n", ++updatacount);
@@ -120,7 +136,7 @@ void HAL_TIM_TriggerCallback(TIM_HandleTypeDef *htim) {
     }
 }
 
-/* DMA半完成中断回调 */
+/* DMA半完成中断回调(溢出更新) */
 void HAL_TIM_PeriodElapsedHalfCpltCallback(TIM_HandleTypeDef *htim) {
     if (htim->Instance == TIM1) {
         u2_prinf("DMA_Half\r\n");
@@ -137,14 +153,38 @@ void HAL_TIM_PeriodElapsedHalfCpltCallback(TIM_HandleTypeDef *htim) {
 }
 
 uint32_t tmp = 0;
-/* 输入捕获中断回调 */
+/* 输入捕获中断回调 DMA完成中断回调 */
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
     if (htim->Instance == TIM1) {
         if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
-            u2_prinf("IC1: %d\r\n", (tmp = HAL_TIM_ReadCapturedValue(&Tim_InitStructure, TIM_CHANNEL_1) + (updatacount * 65535)));
+            Times2 = updatacount;
+            u2_prinf("Times: 2 Interval: %d\r\n", ((dmabuffer[1] + Times2 * 65536) - (dmabuffer[0] + Times1 * 65536)));
+            //u2_prinf("IC1: %d\r\n", (tmp = HAL_TIM_ReadCapturedValue(&Tim_InitStructure, TIM_CHANNEL_1) + (updatacount * 65535)));
         }
         else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2) {
             u2_prinf("IC2: %d\r\n", HAL_TIM_ReadCapturedValue(&Tim_InitStructure, TIM_CHANNEL_2) + (updatacount * 65535) - tmp);
+        }
+    }
+    else if (htim->Instance == TIM2) {
+
+    }
+    else if (htim->Instance == TIM3) {
+
+    }
+    else if (htim->Instance == TIM4) {
+
+    }
+}
+
+/* DMA半完成中断回调(输入捕获) */
+void HAL_TIM_IC_CaptureHalfCpltCallback(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == TIM1) {
+        if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
+            Times1 = updatacount;
+            u2_prinf("Times: 1\r\n");
+        }
+        else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2) {
+
         }
     }
     else if (htim->Instance == TIM2) {
