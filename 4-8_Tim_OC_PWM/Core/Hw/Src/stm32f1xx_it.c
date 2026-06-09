34e23342f51a4d090c0977d39d5d1a1593308d03
Mone <2164711521@qq.com>
2026-06-09 20:42:12 +0800

    PWM轮询

--- 4-8_Tim_OC_PWM/Core/Hw/Src/stm32f1xx_it.c
+++ 4-8_Tim_OC_PWM/Core/Hw/Src/stm32f1xx_it.c
@@ -173,16 +173,4 @@ void USART2_IRQHandler(void) {
 	}
 }
 
-void TIM1_CC_IRQHandler(void) {
-  HAL_TIM_IRQHandler(&Tim_InitStructure);
-}
-
-void TIM1_BRK_IRQHandler(void) {
-  HAL_TIM_IRQHandler(&Tim_InitStructure);
-}
-
-/* void DMA1_Channel4_IRQHandler(void) {
-  HAL_DMA_IRQHandler(&Tim_DMA);
-} */
-
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
