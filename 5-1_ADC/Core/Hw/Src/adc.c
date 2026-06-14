19e60d9990bcbcf9066c8f7250f12fda2c8ee8fd
Mone <2164711521@qq.com>
2026-06-14 16:16:19 +0800

    5-1 ADC

---
+++ 5-1_ADC/Core/Hw/Src/adc.c
@@ -0,0 +1,7 @@
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_adc.h"
+#include "adc.h"
+
+void ADC_Init(void) {
+
+}
---
+++ 5-1_ADC/Core/User/main.c
@@ -0,0 +1,17 @@
+#include "stm32f1xx_hal.h"
+#include "rcc.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_tim.h"
+#include "uart.h"
+#include "tim.h"
+#include <stdint.h>
+
+int main(void) {
+    HAL_Init();
+    RCC_InitClock();
+    UART2_Init();
+
+    while (1) {
+        
+	}
+}
