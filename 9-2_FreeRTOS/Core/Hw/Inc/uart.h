55e3f8b3462dc9b90dea7126d0005c33543b01ff
Mone <2164711521@qq.com>
2026-06-28 22:36:51 +0800

    ---体验抢占式调度，以及时间片轮转与任务优先级的关系--- 1.时间片轮转：同优先级任务之间，每个任务执行完一个时间片之后，主动让出cpu使用权，供同优先级任务执行 2.抢占式调度：不同优先级任务之间，高优先级任务可以直接抢过低优先级任务对cpu的使用权 3.协作式调度：不同优先级任务之间，高优先级任务可以抢在低优先级任务之前执行，但是不会抢断低优先级任务对cpu的使用权，而是会等待低优先级任务执行完一个时间片之后再执行，类似于插队

--- 9-2_FreeRTOS/Core/Hw/Inc/uart.h
+++ 9-2_FreeRTOS/Core/Hw/Inc/uart.h
@@ -1,6 +1,8 @@
 #ifndef __UART_H
 #define __UART_H
 
+extern UART_HandleTypeDef huart1;
+
 void UART_Init(void);
 void u1_printf(char *fmt, ...);
 
--- 9-2_FreeRTOS/Core/Hw/Src/FreeRTOS_Demo.c
+++ 9-2_FreeRTOS/Core/Hw/Src/FreeRTOS_Demo.c
@@ -7,12 +7,18 @@
 #include "task.h" 
 #include "FreeRTOS_Demo.h" 
 #include "LED.h"
+#include "uart.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 // void static task1(void * arg);
 // void static task2(void * arg);
-void static LEDBulinktask(void * pvParameters);
+static void LEDBulinktask(void * pvParameters);
 static void Task1(void * pvParameters);
+static void Task2_PrintString(void * pvParameters);
+static void Task3_PrintString(void * pvParameters);
+static void PrintString(char *Str);
 
 TaskHandle_t task1_handler;
 TaskHandle_t task2_handler;
@@ -27,13 +33,15 @@ LEDBulinkInfo_TypeDef LED1 = {.GPIOx = GPIOA, .GPIO_Pin = GPIO_PIN_1, .time = 20
 LEDBulinkInfo_TypeDef LED2 = {.GPIOx = GPIOA, .GPIO_Pin = GPIO_PIN_2, .time = 1000};
 
 void FreeRTOS_Start(void) {
-    xTaskCreate(LEDBulinktask, "LED1", 128, (void *)&LED1, 4, &task1_handler);
-    xTaskCreate(LEDBulinktask, "LED2", 128, (void *)&LED2, 4, &task2_handler);
-    xTaskCreate(Task1, "Task1", 128, NULL, 4, NULL);
+    xTaskCreate(LEDBulinktask, "LED1", 128, (void *)&LED1, 2, &task1_handler);
+    xTaskCreate(LEDBulinktask, "LED2", 128, (void *)&LED2, 2, &task2_handler);
+    xTaskCreate(Task1, "Task1", 128, NULL, 2, NULL);
+    xTaskCreate(Task2_PrintString, "Task2_PrintString", 128, NULL, 2, NULL);
+    xTaskCreate(Task3_PrintString, "Task3_PrintString", 128, NULL, 3, NULL);
     vTaskStartScheduler();
 }
 
-void static LEDBulinktask(void * pvParameters) {
+static void LEDBulinktask(void * pvParameters) {
     LEDBulinkInfo_TypeDef *info = (LEDBulinkInfo_TypeDef *)pvParameters;
     while (1) {
         HAL_GPIO_WritePin(info->GPIOx, info->GPIO_Pin, GPIO_PIN_RESET);
@@ -55,6 +63,36 @@ static void Task1(void * pvParameters) {
     vTaskDelete(task1_handler);
 
     vTaskDelete(NULL);
+}
+
+static void PrintString(char *Str) {
+    uint16_t len = strlen(Str);
+    uint16_t i = 0;
+
+    while (i < len) {
+        taskENTER_CRITICAL();
+        //vTaskSuspendAll();
+
+        while ((huart1.Instance->SR & USART_SR_TXE) == 0);  // 等待TXE置位
+
+        USART1->DR = Str[i++];
+
+        taskEXIT_CRITICAL(); // taskENTER_CRITICAL() 比 vTaskSuspendAll() 更轻量 — 只关闭中断（保护 USART 寄存器访问），不阻止任务切换
+        //xTaskResumeAll();
+    }
+}
+
+static void Task2_PrintString(void * pvParameters) {
+    while (1) {
+        PrintString("Hello_FreeRTOS");
+    }
+}
+
+static void Task3_PrintString(void * pvParameters) {
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(rand() % 100 + 1));
+        PrintString("0123");
+    }
 }
 
 // void static task1(void * arg) {
