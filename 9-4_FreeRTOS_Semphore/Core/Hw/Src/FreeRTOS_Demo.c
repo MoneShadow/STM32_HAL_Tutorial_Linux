0523f8e7e1e3dcddec34288a92d0a5f120d25890
Mone <2164711521@qq.com>
2026-07-06 15:24:16 +0800

    学习二值信号量的释放与获取

--- 9-4_FreeRTOS_Semphore/Core/Hw/Src/FreeRTOS_Demo.c
+++ 9-4_FreeRTOS_Semphore/Core/Hw/Src/FreeRTOS_Demo.c
@@ -1,19 +1,51 @@
-#include "projdefs.h"
-#include "stm32f103xb.h"
 #include "stm32f1xx_hal.h"
-#include "stm32f1xx_hal_def.h"
-#include "stm32f1xx_hal_gpio.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
-#include "FreeRTOS_Demo.h" 
-#include "LED.h"
-#include "uart.h"
+#include "semphr.h"
+#include "FreeRTOS_Demo.h"
 #include "Key.h"
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
+#include "uart.h"
+
+QueueHandle_t Semaphre_Handler1;
+
+void task_Start(void * pvParameters);
+void task1(void * pvParameters);
+void task2(void * pvParameters);
 
 void FreeRTOS_Start(void) {
+    Semaphre_Handler1 = xSemaphoreCreateBinary();   // 创建二值信号量
+    xTaskCreate(task_Start, "task_Start", 128, NULL, 0, NULL);
+    vTaskStartScheduler();      // 启动调度器
+}
 
+/* 用于创建任务 */
+void task_Start(void * pvParameters) {
+    xTaskCreate(task1, "task1", 128, NULL, 1, NULL);
+    xTaskCreate(task2, "task2", 128, NULL, 2, NULL);
+    vTaskDelete(NULL);
+}
+
+/* task1 检测按键按下->释放信号量 */
+void task1(void * pvParameters) {
+    while (1) {
+        if (Key_ReadStatus(1)) {
+            if (xSemaphoreGive(Semaphre_Handler1) == pdPASS) {
+                u1_printf("Release successful...\r\n");
+            }
+            else {
+                u1_printf("Release failed...\r\n");
+            }
+        }
+        vTaskDelay(10);  // 10ms 轮询间隔，让出 CPU 给空闲任务
+    }
+}
+
+/* task2 获取信号量 */
+void task2(void * pvParameters) {
+    while (1) {
+        if (xSemaphoreTake(Semaphre_Handler1, portMAX_DELAY) == pdPASS) {
+            u1_printf("obtain successful...\r\n");
+        }
+    }
 }
--- 9-4_FreeRTOS_Semphore/Core/User/main.c
+++ 9-4_FreeRTOS_Semphore/Core/User/main.c
@@ -15,21 +15,12 @@ int main(void) {
     LED_Init();
     UART_Init();
     Key_Init();
-    Tim_Init();
 
     // u1_printf("Hello FreeRTOS\r\n");
 
     FreeRTOS_Start();
 
     while (1) {
-        HAL_Delay(5000);
-        u1_printf("Disable interrupt...\r\n");
-        portDISABLE_INTERRUPTS();
-        // 关中断后不能用 HAL_Delay（依赖 SysTick），改用忙等待 ~3s
-        for (volatile uint32_t i = 0; i < 9000000; i++) {
-            __NOP();
-        }
-        u1_printf("Enable interrupts...\r\n");
-        portENABLE_INTERRUPTS();
+        
     }
 }
