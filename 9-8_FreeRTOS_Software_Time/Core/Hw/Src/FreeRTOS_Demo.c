d6269d7d7507fabfee71921e571968b3511a9433
Mone <2164711521@qq.com>
2026-07-11 18:23:50 +0800

    软件定时器(单次和周期)

--- 9-8_FreeRTOS_Software_Time/Core/Hw/Src/FreeRTOS_Demo.c
+++ 9-8_FreeRTOS_Software_Time/Core/Hw/Src/FreeRTOS_Demo.c
@@ -9,11 +9,13 @@
 #include "uart.h"
 
 TaskHandle_t Task1_Handler;
-TaskHandle_t Task2_Handler;
+TimerHandle_t Timer1_Handler;
+TimerHandle_t Timer2_Handler;
 
 void task_Start(void * pvParameters);
 void task1(void * pvParameters);
-void task2(void * pvParameters);
+void timer1_calback( TimerHandle_t pxTimer );
+void timer2_calback( TimerHandle_t pxTimer );
 
 void FreeRTOS_Start(void) {
     xTaskCreate(task_Start, "task_Start", 128, NULL, 0, NULL);
@@ -24,31 +26,30 @@ void FreeRTOS_Start(void) {
 void task_Start(void * pvParameters) {
     taskENTER_CRITICAL();
 
+    /* 创建软件定时器 */
+    Timer1_Handler = xTimerCreate("Timer1", 1000, pdFALSE, (void *)1, timer1_calback);   // 单次
+    Timer2_Handler = xTimerCreate("Timer2", 1000, pdTRUE, (void *)2, timer2_calback);    // 周期循环
+
     xTaskCreate(task1, "task1", 128, NULL, 1, &Task1_Handler);
-    xTaskCreate(task2, "task2", 128, NULL, 2, &Task2_Handler);
 
     taskEXIT_CRITICAL();
 
     vTaskDelete(NULL);
 }
 
-/* task1 按键1按下->释放任务通知模拟。。。 */
+/* task1 按键1按下->开启软件定时器 按键2按下->关闭软件定时器 */
 void task1(void * pvParameters) {
     uint8_t Key_Num = 0;
     while (1) {
         Key_Num = Key_Scan();
         switch (Key_Num) {
             case 1:
-                xTaskNotify(Task2_Handler, (1 << 0), eSetBits);
-                u1_printf("Bit0 -> 1\r\n");
+                xTimerStart(Timer1_Handler, portMAX_DELAY);
+                xTimerStart(Timer2_Handler, portMAX_DELAY);
                 break;
             case 2:
-                xTaskNotify(Task2_Handler, (1 << 1), eSetValueWithOverwrite);
-                u1_printf("Bit1 -> 1\r\n");
-                break;
-            case 3:
-                xTaskNotify(Task2_Handler, (1 << 2), eSetValueWithOverwrite);
-                u1_printf("Bit2 -> 1...\r\n");
+                xTimerStop(Timer1_Handler, portMAX_DELAY);
+                xTimerStop(Timer2_Handler, portMAX_DELAY);
                 break;
             default:
                 vTaskDelay(pdMS_TO_TICKS(10));
@@ -56,23 +57,12 @@ void task1(void * pvParameters) {
     }
 }
 
-/* task2 接收任务通知模拟。。。 */
-void task2(void * pvParameters) {
-    uint32_t Value = 0, event_bit = 0;
-    while (1) {
-        xTaskNotifyWait(0, 0xFFFFFFFF, &Value, portMAX_DELAY);
-        if (Value & (1 << 0)) {
-            event_bit |= (1 << 0);
-        }
-        if (Value & (1 << 1)) {
-            event_bit |= (1 << 1);
-        }
-        if (Value & (1 << 2)) {
-            event_bit |= (1 << 2);
-        }
-        if (event_bit == 0x0007) {
-            u1_printf("Simulate Event...\r\n");
-            event_bit = 0;
-        }
-    }
-}
+void timer1_calback( TimerHandle_t pxTimer ) {
+    static uint8_t NUM1 = 0;
+    u1_printf("Timer1: %d\r\n", ++NUM1);
+ }
+
+void timer2_calback( TimerHandle_t pxTimer ) {
+    static uint8_t NUM2 = 0;
+    u1_printf("Timer2: %d\r\n", ++NUM2);
+ }
